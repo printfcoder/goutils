cb10fd81393152e6b349b48a2a09e8f679c66015
shuxian <printfcoder@gmail.com>
2018-09-26 13:58:18 +0800

    NewPwd

--- crypto/cryptoutils.go
+++ crypto/cryptoutils.go
@@ -3,8 +3,8 @@ package cryptoutils
 import "golang.org/x/crypto/bcrypt"
 
 // NewPwd creates a new pwd with salt
-func NewPwd(plainPWD string) (pwd string, err error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(plainPWD), 14)
+func NewPwd(plainPWD string, cost int) (pwd string, err error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(plainPWD), cost)
 	return string(bytes), err
 }
 
--- test/crypto_test.go
+++ test/crypto_test.go
@@ -7,15 +7,15 @@ import (
 )
 
 func TestNewPwd(t *testing.T) {
-	out, err := cryptoutils.NewPwd("1234567")
+	out, err := cryptoutils.NewPwd("1234567", 10)
 	assert.Nil(t, err)
 	t.Log(out)
 }
 
 func TestIsPwdOK(t *testing.T) {
 
-	testStr := "$2a$14$SiorEqyfbs8GEWg1OyrLE.61aOUA69BX0GVMoYKU7MzjbsWPuntCS"
-	out := cryptoutils.IsPwdOK("123456", testStr)
+	testStr := "$2a$10$vJ.4bVx.ZmDXmyR.Ayj//OvC0grffAOYRq4LoQiwT/A3JUqVPrNHS"
+	out := cryptoutils.IsPwdOK("1234567", testStr)
 	assert.True(t, out)
 
 	testStr = "$2a$14$k.DOsS3fhvKAnrq0FsciZufrAELJKykeGuRH7wsSoQ7DWgJc.b3.2"
