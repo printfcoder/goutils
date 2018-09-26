1087cd242b7f0c1916b2f8caf77ae1259bb5b5f0
shuxian <printfcoder@gmail.com>
2018-09-26 11:58:58 +0800

    pwd

---
+++ crypto/cryptoutils.go
@@ -0,0 +1,17 @@
+package crypto
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// NewPwd creates a new pwd with salt
+func NewPwd(plainPWD string) (pwd string, err error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(plainPWD), 14)
+	return string(bytes), err
+}
+
+// IsPwdOK checks plainPWD and plainPWD are matching
+func IsPwdOK(plainPWD, encryptedPwd string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(encryptedPwd), []byte(plainPWD))
+	return err == nil
+}
